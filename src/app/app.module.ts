ea7fb58625911b91ce8b78f6c617da342926b9f0
Suganya <suknyat1995@gmail.com>
2020-06-28 21:23:25 +0530

    Upload UI

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DndDirective } from './dnd.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DndDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
